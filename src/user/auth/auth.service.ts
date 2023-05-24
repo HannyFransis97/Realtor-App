4cc03afb678d484d5174cab6063332c033bfbaa7
Hanny Fransis <hanny.fransis@gmail.com>
2023-05-24 13:35:37 +0400

    init the project, connect to database, implement Authentication

---
+++ src/user/auth/auth.service.ts
@@ -0,0 +1,89 @@
+import { ConflictException, HttpException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcryptjs';
+import { UserType } from '@prisma/client';
+import * as jwt from 'jsonwebtoken';
+
+interface SignupParams {
+  email: string;
+  password: string;
+  name: string;
+  phone: string;
+}
+
+interface SigninParams {
+  email: string;
+  password: string;
+}
+
+@Injectable()
+export class AuthService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async signup(
+    { email, password, name, phone }: SignupParams,
+    userType: UserType,
+  ) {
+    const userExists = await this.prismaService.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (userExists) {
+      throw new ConflictException();
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await this.prismaService.user.create({
+      data: {
+        email,
+        name,
+        phone,
+        password: hashedPassword,
+        user_type: userType,
+      },
+    });
+    const token = await jwt.sign(
+      {
+        name,
+        id: user.id,
+      },
+      process.env.JSON_TOKEN_KEY,
+      {
+        expiresIn: 36000,
+      },
+    );
+    return { token };
+  }
+
+  async signin({ email, password }: SigninParams) {
+    const user = await this.prismaService.user.findUnique({
+      where: { email },
+    });
+    if (!user) {
+      throw new HttpException('Invalid  credentials', 400);
+    }
+
+    const hashedPassword = user.password;
+    const isValidPassword = await bcrypt.compare(password, hashedPassword);
+
+    if (!isValidPassword) throw new HttpException('Invalid  credentials', 400);
+
+    const token = await jwt.sign(
+      {
+        name: user.name,
+        id: user.id,
+      },
+      process.env.JSON_TOKEN_KEY,
+      {
+        expiresIn: 36000,
+      },
+    );
+    return { token };
+  }
+  generateProductKey(email: string, userType: UserType) {
+    const string = `${email}-${userType}-${process.env.PRODUCT_KEY_SECRET}`;
+
+    return bcrypt.hash(string, 10);
+  }
+}
